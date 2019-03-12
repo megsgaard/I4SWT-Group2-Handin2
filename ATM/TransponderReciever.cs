98193f57b9b54076d4312534537f426f5e32a6cf
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2019-03-12 13:15:41 +0100

    Gjort så TransponderReciever arver fra ITransponderReciever

--- ATM/TransponderReciever.cs
+++ ATM/TransponderReciever.cs
@@ -1,6 +1,13 @@
-﻿namespace ATM
+﻿using System;
+
+namespace ATM
 {
     public class TransponderReciever : ITransponderReciever
     {
+       public event EventHandler<ITrackReciever> NewTransponderRecieverEvent;
+       public void DataReady(object RawTransponderDataEventArgs)
+       {
+          throw new NotImplementedException();
+       }
     }
 }
