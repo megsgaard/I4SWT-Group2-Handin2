0a7a3238749050eb705df0fb25e9735f198ec63a
NannaD <201706641@post.au.dk>
2019-03-12 13:43:59 +0100

    Added metoder i AirTrafficController og gjort klassen public

--- ATM/AirTrafficController.cs
+++ ATM/AirTrafficController.cs
@@ -8,7 +8,25 @@ namespace ATM
 {
     public class AirTrafficController
     {
+        public void InvestigateInfo(ISeperationChecker seperationChecker)
+        {
 
+        }
+
+        public void Calculate(IDataCalculator dataCalculator)
+        {
+
+        }
+
+        public void Print(IConditionViewer conditionViewer)
+        {
+
+        }
+
+        public void RecieveCalculatedEvent()
+        {
+
+        }
 
     }
 }
--- ATM/IConditionViewer.cs
+++ ATM/IConditionViewer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-   class IConditionViewer
+   public class IConditionViewer
    {
    }
 }
