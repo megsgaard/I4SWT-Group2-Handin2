c7000d7064ae4cc6c78cc3aae9b3ba819e41a7c4
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2019-03-12 13:45:51 +0100

    Gået i gang med implementering af DataCalculator

--- ATM/DataCalculator.cs
+++ ATM/DataCalculator.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-   class DataCalculator
+   public class DataCalculator:IDataCalculator
    {
+      public event EventHandler<List<TrackInfo>> CalculateEvent;
+      public void DoCalculations()
+      {
+         CompareLists();
+         CalculateCourse();
+         CalculateVelocity();
+      }
+
+      public void CalculateVelocity()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void CalculateCourse()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void CompareLists()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void RecieveTrackEvent(object sender, TrackInfo e)
+      {
+         throw new NotImplementedException();
+      }
    }
 }
