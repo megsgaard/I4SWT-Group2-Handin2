ee46d49898dc36981dff453b06df82446fe78fba
megsgaard <mathias.egsgaard@post.au.dk>
2019-03-13 11:44:25 +0100

    Fjernet TransponderReciever og begyndt på implementering af TrackReceiver

--- ATM/TrackReciever.cs
+++ ATM/TrackReciever.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace ATM
 {
-   public class TrackReciever
-   {
-   }
+    public class TrackReciever
+    {
+        private List<TrackInfo> trackInfoList;
+        private ITransponderReceiver reciever;
+
+        public TrackReciever(ITransponderReceiver reciever)
+        {
+            this.reciever = reciever;
+
+            this.reciever.TransponderDataReady += Reciever_TransponderDataReady;
+        }
+
+        private void Reciever_TransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
